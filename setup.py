c766f8ea129a1a901f29697903a6d66e658b5534
Jakub Narębski <jnareb@mat.umk.pl>
2025-12-08 00:31:15 +0100

    Move from setup.py to pyproject.toml, leaving a stub
    The setup.py file could have been left as it was before, but it
    leads to data duplication (with possible mismatch in the future):
    
    https://packaging.python.org/en/latest/guides/modernize-setup-py-project/
    
    This new pyproject.toml tries to be 1-to-1 representation of old
    setup.py, just with some 'classifiers' added (empty list in setup.py).

--- setup.py
+++ setup.py
@@ -1,29 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
 from linguist import VERSION
 
 
 setup(name='linguist',
       version=VERSION,
-      keywords=('linguist', 'detect', 'programming', 'language'),
-      description='Language Savant',
-      long_description='Language Savant',
-      license='New BSD',
-
-      url='https://github.com/retanoj/linguist',
-      author='retanoj',
-      author_email='',
-
-      packages=find_packages(),
-      include_package_data=True,
-      platforms='any',
-      install_requires=['PyYAML',
-                        'six',
-                        'pygments>=2.5.2',
-                        'pygments-github-lexers>=0.0.3',
-                        'charlockholmes',
-                        'mime>=0.1.0'],
-      classifiers=[],
-      scripts=['bin/pylinguist'])
+)
